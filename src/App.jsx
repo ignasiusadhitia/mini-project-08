be99dda5e8f8616ac70c851f292c258d661f3538
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-11-29 11:56:02 +0700

    chore: configure routing and add initial project structures

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import { About, Blog, BlogDetails, Home, Newsletter, NotFound } from '@pages';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<Blog />} path="/blog" />
+        <Route element={<BlogDetails />} path="/blog/:key" />
+        <Route element={<About />} path="/about" />
+        <Route element={<Newsletter />} path="/contact" />
+        <Route element={<NotFound />} path="*" />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as About } from './About';
+export { default as Blog } from './Blog';
+export { default as BlogDetails } from './BlogDetails';
+export { default as Home } from './Home';
+export { default as Newsletter } from './Newsletter';
+export { default as NotFound } from './NotFound';
