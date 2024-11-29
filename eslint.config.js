40459e87a9a63106e1fe3ad9c095e80db27db0fd
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-11-29 11:45:40 +0700

    chore: configure eslint, prettier and project folders

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import eslintPluginImport from 'eslint-plugin-import';
+import prettier from 'eslint-plugin-prettier';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
 
 export default [
   { ignores: ['dist'] },
@@ -17,22 +20,105 @@ export default [
         sourceType: 'module',
       },
     },
-    settings: { react: { version: '18.3' } },
+    settings: {
+      react: { version: '18.3' },
+      'import/resolver': {
+        alias: {
+          map: [
+            ['@', './src'],
+            ['@assets', './src/assets'],
+            ['@components', './src/components'],
+            ['@hooks', './src/hooks'],
+            ['@pages', './src/pages'],
+            ['@redux', './src/redux'],
+            ['@services', './src/services'],
+            ['@utils', './src/utils'],
+          ],
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
+    },
     plugins: {
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      import: eslintPluginImport,
+      prettier, // Menambahkan prettier ke dalam plugin
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+
+      // Prettier integration (disables conflicting rules)
+      ...prettierConfig.rules,
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          semi: true,
+          printWidth: 80,
+          trailingComma: 'es5',
+          jsxSingleQuote: false,
+          arrowParens: 'always',
+        },
+      ],
+
+      // Custom rules
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'no-unused-vars': [
+        'error',
+        {
+          varsIgnorePattern: '^React$',
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin', // Node.js modules
+            'external', // npm modules
+            'internal', // Aliases
+            ['parent', 'sibling', 'index'], // Relative imports
+            'unknown', // Other cases
+          ],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      'react/jsx-sort-props': [
+        'error',
+        {
+          callbacksLast: true,
+          shorthandFirst: true,
+          noSortAlphabetically: false,
+          reservedFirst: true,
+        },
+      ],
+      'react/jsx-curly-newline': [
+        'error',
+        {
+          multiline: 'consistent',
+          singleline: 'consistent',
+        },
+      ],
+      'react/jsx-no-duplicate-props': ['error', { ignoreCase: true }],
     },
   },
-]
+];
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import svgr from "vite-plugin-svgr";
+import path from 'path';
+
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      '@': path.resolve('src'),
+      '@pages': path.resolve('src/pages'),
+      '@components': path.resolve('src/components'),
+      '@redux': path.resolve('src/redux'),
+      '@utils': path.resolve('src/utils'),
+      '@assets': path.resolve('src/assets'),
+      '@hooks': path.resolve('src/hooks'),
+      '@services': path.resolve('src/services'),
+    },
+  },
 });
