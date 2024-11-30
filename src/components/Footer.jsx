a999cfd107668b3e57e6fa6ac60d7447aa1e5a61
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-11-30 15:58:12 +0700

    fix(footer-page): add margin top

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,13 +23,13 @@ const navLinks = [
 
 const Footer = () => {
   return (
-    <footer className="container flex text-xl leading-6 text-black-2 gap-3.5">
+    <footer className="container flex flex-col-reverse items-center md:flex-row md:items-start text-xl leading-6 text-black-2 gap-3.5 py-7.5 px-8 lg:px-0 mt-19">
       {/* Copyright */}
-      <span>&copy; 2024</span>
+      <span className="mt-4 md:mt-0">&copy; 2024</span>
 
       {/* Navigation */}
       <nav>
-        <ul className="flex gap-3.5">
+        <ul className="flex flex-col items-center md:flex-row gap-3.5">
           {navLinks.map(({ name, path }) => (
             <li key={name}>
               <a href={path} rel="noreferrer noopener" target="_black">
