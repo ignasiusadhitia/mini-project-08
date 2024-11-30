24b89e2c6756dbf2d2a358936253959d2ee5016e
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-11-30 16:29:38 +0700

    feat(navbar-component): add mobile menu

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,3 +1,5 @@
 export { default as ArrowLink } from './arrow-link.svg?react';
+export { default as Close } from './close.svg?react';
+export { default as Hamburger } from './hamburger.svg?react';
 export { default as Moon } from './moon.svg?react';
 export { default as Sun } from './sun.svg?react';
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import { Link } from 'react-router-dom';
 
-import { Moon, Sun } from '@icons';
+import { Hamburger, Moon, Sun, Close } from '@icons';
 
 const navLinks = [
   {
@@ -21,41 +21,85 @@ const navLinks = [
 
 const Navbar = () => {
   const [theme, setTheme] = useState('light');
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleTheme = () => {
     setTheme(theme === 'light' ? 'dark' : 'light');
   };
-  return (
-    <nav className="container flex items-center justify-between py-2.5 mt-7.5 mb-5 text-black-2">
-      {/* Logo */}
-      <Link to="/">
-        <span className="font-semibold text-lg leading-6">LumosBlog</span>
-      </Link>
 
-      <div className="flex items-center gap-3.5">
-        {/* Navigation Links */}
-        <ul className="flex items-center gap-3.5">
-          {navLinks.map((link) => (
-            <li key={link.label} className="text-lg leading-6 p-2">
-              <Link to={link.href}>{link.label}</Link>
-            </li>
-          ))}
-        </ul>
-        {/* Theme Toggle */}
-        <div
-          className={`flex items-center relative py-2 px-4 gap-4 rounded-3.5xl transition-all duration-300 ease-in-out ${theme === 'light' ? ' bg-black-4' : ' bg-white'}`}
-        >
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+  return (
+    <>
+      <nav className="container flex items-center justify-between py-2.5 mt-7.5 mb-5 px-8 lg:px-0 text-black-2">
+        {/* Logo */}
+        <Link to="/">
+          <span className="font-semibold text-lg leading-6">LumosBlog</span>
+        </Link>
+
+        {/* Tablet and Desktop Navigation */}
+        <div className="hidden md:flex items-center gap-3.5">
+          {/* Navigation Links */}
+          <ul className="flex items-center gap-3.5">
+            {navLinks.map((link) => (
+              <li key={link.label} className="text-lg leading-6 p-2">
+                <Link to={link.href}>{link.label}</Link>
+              </li>
+            ))}
+          </ul>
+          {/* Theme Toggle */}
           <div
-            className={`w-6 h-6 rounded-full  absolute cursor-pointer transition-all duration-300 ease-in-out ${
-              theme === 'light' ? 'left-14 bg-white' : 'left-4 bg-black-4'
-            }`}
-            onClick={toggleTheme}
-          ></div>
-          <Sun className="cursor-pointer" onClick={toggleTheme} />
-          <Moon className="cursor-pointer" onClick={toggleTheme} />
+            className={`flex items-center relative py-2 px-4 gap-4 rounded-3.5xl transition-all duration-300 ease-in-out ${theme === 'light' ? ' bg-black-4' : ' bg-white'}`}
+          >
+            <div
+              className={`w-6 h-6 rounded-full  absolute cursor-pointer transition-all duration-300 ease-in-out ${
+                theme === 'light' ? 'left-14 bg-white' : 'left-4 bg-black-4'
+              }`}
+              onClick={toggleTheme}
+            ></div>
+            <Sun className="cursor-pointer" onClick={toggleTheme} />
+            <Moon className="cursor-pointer" onClick={toggleTheme} />
+          </div>
         </div>
-      </div>
-    </nav>
+
+        {/* Mobile Navigation Toggle */}
+        <Hamburger className="md:hidden" onClick={toggleMenu} />
+      </nav>
+
+      {/* Mobile Navigation */}
+      {isOpen && (
+        <>
+          <nav className="flex flex-col absolute md:hidden items-center justify-center gap-3.5 w-full bg-white top-0 h-screen">
+            {/* Navigation Links */}
+            <ul className="flex flex-col items-center gap-3.5">
+              {navLinks.map((link) => (
+                <li key={link.label} className="text-lg leading-6 p-2">
+                  <Link to={link.href}>{link.label}</Link>
+                </li>
+              ))}
+            </ul>
+            {/* Theme Toggle */}
+            <div
+              className={`flex items-center relative py-2 px-4 gap-4 rounded-3.5xl transition-all duration-300 ease-in-out ${theme === 'light' ? ' bg-black-4' : ' bg-white'}`}
+            >
+              <div
+                className={`w-6 h-6 rounded-full  absolute cursor-pointer transition-all duration-300 ease-in-out ${
+                  theme === 'light' ? 'left-14 bg-white' : 'left-4 bg-black-4'
+                }`}
+                onClick={toggleTheme}
+              ></div>
+              <Sun className="cursor-pointer" onClick={toggleTheme} />
+              <Moon className="cursor-pointer" onClick={toggleTheme} />
+            </div>
+          </nav>
+          <Close
+            className={`absolute bottom-4 left-0 right-0 mx-auto`}
+            onClick={toggleMenu}
+          />
+        </>
+      )}
+    </>
   );
 };
 
