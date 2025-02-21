7cf5bc51d38c871ce0e7322393e10cb1624a93f8
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-02-21 11:21:59 +0700

    docs: add comment on postcard component

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import { PostLabel } from '@components';
 import { ArrowLink } from '@icons';
 
+// Post card component
+
 const PostCard = ({
   margin,
   display,
